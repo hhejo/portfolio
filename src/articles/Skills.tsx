fac97a146b3bbe31a412feec70f9542419db5ee1
hhejo <litsmainjof@gmail.com>
2024-11-25 04:59:33 +0900

    feat: update ul

--- src/articles/Skills.tsx
+++ src/articles/Skills.tsx
@@ -11,21 +11,21 @@ export default function Skills() {
           <div className="flex items-center">
             <div className="w-36 text-2xl font-bold text-gray-700">Language</div>
             <div className="flex flex-col gap-1">
-              <div className="flex gap-1">
+              <ul className="flex gap-1">
                 <SkillItem>TypeScript</SkillItem>
                 <SkillItem>JavaScript</SkillItem>
                 <SkillItem>Python</SkillItem>
                 <SkillItem>C#</SkillItem>
                 <SkillItem>C</SkillItem>
                 <SkillItem>C++</SkillItem>
-              </div>
+              </ul>
             </div>
           </div>
           <div className="flex items-center">
             <div className="w-36"></div>
-            <div className="flex gap-1">
+            <ul className="flex gap-1">
               <SkillItem>Rust</SkillItem>
-            </div>
+            </ul>
           </div>
         </div>
 
@@ -34,24 +34,24 @@ export default function Skills() {
           <div className="flex items-center">
             <div className="w-36 text-2xl font-bold text-gray-700">Client</div>
             <div className="flex flex-col gap-1">
-              <div className="flex gap-1">
+              <ul className="flex gap-1">
                 <SkillItem>React</SkillItem>
                 <SkillItem>Next</SkillItem>
                 <SkillItem>Vite</SkillItem>
                 <SkillItem>WebPack</SkillItem>
                 <SkillItem>Vue</SkillItem>
-              </div>
+              </ul>
             </div>
           </div>
           <div className="flex items-center">
             <div className="w-36"></div>
-            <div className="flex gap-1">
+            <ul className="flex gap-1">
               <SkillItem>React Router</SkillItem>
               <SkillItem>Redux Toolkit</SkillItem>
               <SkillItem>Zustand</SkillItem>
               <SkillItem>TanStack Query</SkillItem>
               <SkillItem>Immer</SkillItem>
-            </div>
+            </ul>
           </div>
         </div>
 
@@ -60,22 +60,22 @@ export default function Skills() {
           <div className="flex items-center">
             <div className="w-36 text-2xl font-bold text-gray-700">Server</div>
             <div className="flex flex-col gap-1">
-              <div className="flex gap-1">
+              <ul className="flex gap-1">
                 <SkillItem>Express</SkillItem>
                 <SkillItem>Django</SkillItem>
                 <SkillItem>Flask</SkillItem>
                 <SkillItem>ASP .NET Core</SkillItem>
-              </div>
+              </ul>
             </div>
           </div>
           <div className="flex items-center">
             <div className="w-36"></div>
-            <div className="flex gap-1">
+            <ul className="flex gap-1">
               {/* <SkillItem>Sequelize</SkillItem>
               <SkillItem>Mongoose</SkillItem> */}
               <SkillItem>JWT</SkillItem>
               <SkillItem>OAuth</SkillItem>
-            </div>
+            </ul>
           </div>
         </div>
 
@@ -84,53 +84,53 @@ export default function Skills() {
           <div className="flex items-center">
             <div className="w-36 text-2xl font-bold text-gray-700">Database</div>
             <div className="flex flex-col gap-1">
-              <div className="flex gap-1">
+              <ul className="flex gap-1">
                 <SkillItem>MySQL</SkillItem>
                 <SkillItem>MongoDB</SkillItem>
                 <SkillItem>SQLite</SkillItem>
                 <SkillItem>Supabase</SkillItem>
                 <SkillItem>Firebase</SkillItem>
-              </div>
+              </ul>
             </div>
           </div>
           <div className="flex items-center">
             <div className="w-36"></div>
-            <div className="flex gap-1">
+            <ul className="flex gap-1">
               <SkillItem>Sequelize</SkillItem>
               <SkillItem>Mongoose</SkillItem>
-            </div>
+            </ul>
           </div>
         </div>
 
         {/* UI */}
         {/* <div className="flex items-center">
           <div className="w-36 text-2xl font-bold text-gray-700">UI</div>
-          <div className="flex gap-1">
+          <ul className="flex gap-1">
             <SkillItem>Tailwind CSS</SkillItem>
             <SkillItem>SASS</SkillItem>
             <SkillItem>Styled Components</SkillItem>
             <SkillItem>Bootstrap</SkillItem>
             <SkillItem>MUI</SkillItem>
-          </div>
+          </ul>
         </div> */}
 
         <div>
           <div className="flex items-center">
             <div className="w-36 text-2xl font-bold text-gray-700">UI</div>
             <div className="flex flex-col gap-1">
-              <div className="flex gap-1">
+              <ul className="flex gap-1">
                 <SkillItem>Tailwind CSS</SkillItem>
-              </div>
+              </ul>
             </div>
           </div>
           <div className="flex items-center">
             <div className="w-36"></div>
-            <div className="flex gap-1">
+            <ul className="flex gap-1">
               <SkillItem>SASS</SkillItem>
               <SkillItem>Styled Components</SkillItem>
               <SkillItem>Bootstrap</SkillItem>
               <SkillItem>MUI</SkillItem>
-            </div>
+            </ul>
           </div>
         </div>
 
--- src/components/SkillItem.tsx
+++ src/components/SkillItem.tsx
@@ -1,5 +1,5 @@
 export const SkillItem = ({ children }: { children: string }) => (
-  <span className="px-2.5 py-0.5 rounded-lg flex justify-center items-center text-sm bg-gray-800 text-gray-100">
+  <li className="px-2.5 py-0.5 rounded-lg flex justify-center items-center text-sm bg-gray-800 text-gray-100">
     {children}
-  </span>
+  </li>
 );
