f5fd0291501fbd52ab800ee8548c036dfc64aa4f
hhejo <litsmainjof@gmail.com>
2024-11-24 20:41:27 +0900

    feat: update font

--- src/articles/Header.tsx
+++ src/articles/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <header className="h-16 flex justify-between items-center px-8">
-      <a href="/" className="text-lg font-bold text-gray-900">
+      <a href="/" className="text-xl font-bold text-gray-900">
         HHEJO'S PORTFOLIO
       </a>
-      <nav className="flex gap-6 text-gray-700">
+      <nav className="flex gap-6 text-sm text-gray-700">
         <a href="#">ABOUT ME</a>
         <a href="#">SKILLS</a>
         <a href="#">PROJECTS</a>
