1a09cfd634977af9abe8613fc8e08a26043fadc4
hhejo <litsmainjof@gmail.com>
2024-11-24 16:07:07 +0900

    feat: add masthead

---
+++ src/articles/Masthead.tsx
@@ -0,0 +1,20 @@
+export default function Masthead() {
+  return (
+    <article className="flex flex-col justify-center items-center py-28 bg-gray-200 gap-12">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <p className="text-4xl">함희주</p>
+        <p className="text-4xl">개발자 포트폴리오</p>
+      </div>
+
+      <hr className="h-1 w-12 bg-sky-400" />
+
+      <div className="flex flex-col justify-center items-center text-xl">
+        <p>안녕하세요</p>
+        <p>기술에 도전하는 개발자</p>
+        <p>함희주입니다.</p>
+      </div>
+
+      <button className="w-32 h-12 bg-sky-400 text-white rounded-3xl">더 알아보기</button>
+    </article>
+  );
+}
