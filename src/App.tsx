6ce4bcf3a9afe0d0d0bc214b6fee2a1ee5a6c8b9
hhejo <litsmainjof@gmail.com>
2024-11-24 16:07:17 +0900

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import Nav from './components/Nav';
 import Masthead from './articles/Masthead';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <>
       <Nav />
       <Masthead />
-      <article>자기소개, 사진</article>
-      <article>기술 스택</article>
-      <article>아카이브, 링크 모음</article>
-      <article>프로젝트</article>
-      <article>커리어, 교육 목록</article>
     </>
   );
 }
