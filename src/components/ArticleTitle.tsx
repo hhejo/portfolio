7ee7ca9a52163e72a137fef5ea6c2f7429e3fc0a
hhejo <litsmainjof@gmail.com>
2024-11-26 06:53:05 +0900

    feat: update letter spacing

--- src/components/ArticleTitle.tsx
+++ src/components/ArticleTitle.tsx
@@ -1,3 +1,5 @@
 export const ArticleTitle = ({ children }: { children: string }) => (
-  <h2 className="text-4xl font-bold pb-2 border-b-2 border-b-gray-400 text-gray-900">{children}</h2>
+  <h2 className="text-4xl font-bold tracking-wider pb-2 border-b-2 border-b-gray-400 text-gray-900">
+    {children}
+  </h2>
 );
