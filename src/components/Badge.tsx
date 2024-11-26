9f34bdb375956793410c6e96e955eb15d47ca4a3
hhejo <litsmainjof@gmail.com>
2024-11-26 10:13:12 +0900

    feat: add badge status

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,5 +1,9 @@
-export const Badge = ({ children }: { children: string }) => (
-  <li className="px-2.5 py-0.5 rounded-lg flex justify-center items-center text-sm bg-gray-800 text-gray-100">
+export const Badge = ({ children, reverse = false }: { children: string; reverse?: boolean }) => (
+  <li
+    className={`px-2.5 py-0.5 rounded-lg flex justify-center items-center text-sm ${
+      reverse ? 'bg-white text-gray-700' : 'bg-gray-700 text-gray-50'
+    }`}
+  >
     {children}
   </li>
 );
