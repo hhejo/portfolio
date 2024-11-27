756288bc34c5197063ef7690ee1021a102cb7f1d
hhejo <litsmainjof@gmail.com>
2024-11-27 16:02:06 +0900

    feat: Add bars menu

--- src/articles/Header.tsx
+++ src/articles/Header.tsx
@@ -1,15 +1,40 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+
   return (
-    <header className="h-16 w-full flex justify-between items-center px-8">
-      <a href="/" className="text-xl font-bold text-gray-900">
-        HHEJO'S PORTFOLIO
-      </a>
-      <nav className="flex gap-6 text-sm text-gray-700">
-        <a href="#">ABOUT ME</a>
-        <a href="#">SKILLS</a>
-        <a href="#">PROJECTS</a>
-        <a href="#">CAREER</a>
-      </nav>
-    </header>
+    <>
+      <header className="h-16 w-full flex justify-between items-center px-8">
+        <a href="/" className="text-xl font-bold text-gray-900">
+          HHEJO'S PORTFOLIO
+        </a>
+
+        <nav className="hidden sm:flex gap-6 text-sm text-gray-700">
+          <a href="#">ABOUT ME</a>
+          <a href="#">SKILLS</a>
+          <a href="#">PROJECTS</a>
+          <a href="#">CAREER</a>
+        </nav>
+
+        <nav className="sm:hidden">
+          <button onClick={toggleMenu}>
+            {isOpen ? <></> : <FontAwesomeIcon icon={faBars} size="xl" />}
+          </button>
+        </nav>
+      </header>
+
+      {isOpen && (
+        <div className="sm:hidden flex flex-col gap-3 items-center w-full text-xl text-gray-700">
+          <a href="#">ABOUT ME</a>
+          <a href="#">SKILLS</a>
+          <a href="#">PROJECTS</a>
+          <a href="#">CAREER</a>
+        </div>
+      )}
+    </>
   );
 }
