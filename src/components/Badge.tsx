db3f2a871f5fe35f1a32ae24dbdcfc181389acd6
hhejo <litsmainjof@gmail.com>
2024-11-27 12:22:59 +0900

    feat: Update badge team

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,7 +1,20 @@
-export const Badge = ({ children, reverse = false }: { children: string; reverse?: boolean }) => (
+export const Badge = ({
+  children,
+  // reverse = false,
+  team = true,
+}: {
+  children: string;
+  reverse?: boolean;
+  team?: boolean;
+}) => (
+  // <li
+  //   className={`px-2.5 py-0.5 rounded-lg flex justify-center items-center text-sm cursor-pointer ${
+  //     reverse ? 'bg-gray-100 text-gray-700' : 'bg-gray-700 text-gray-50'
+  //   } ${team ? 'bg-gray-700 text-gray-50' : 'bg-gray-100 text-gray-700 '}`}
+  // >
   <li
-    className={`px-2.5 py-0.5 rounded-lg flex justify-center items-center text-sm ${
-      reverse ? 'bg-white text-gray-700' : 'bg-gray-700 text-gray-50'
+    className={`px-2.5 py-0.5 rounded-lg flex justify-center items-center text-sm cursor-pointer ${
+      team ? 'bg-gray-700 text-gray-50' : 'bg-gray-100 text-gray-700 '
     }`}
   >
     {children}
