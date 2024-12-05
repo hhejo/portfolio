0ae26e428b06833761927201a4f64605eb0141e6
hhejo <litsmainjof@gmail.com>
2024-12-05 23:14:41 +0900

    style: Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import { Header, Masthead, AboutMe, Skills, Projects, Career } from './articles';
 
-// import Header from './articles/Header';
-// import Masthead from './articles/Masthead';
-// import AboutMe from './articles/AboutMe';
-// import Skills from './articles/Skills';
-// import Projects from './articles/Projects';
-// import Career from './articles/Career';
-
 function App() {
   return (
     <>
